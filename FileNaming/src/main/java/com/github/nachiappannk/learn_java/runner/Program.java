a22f136e6435b3dd2cac27ce05795172887145ee
Nachiappan NK <nachiappan.nk@mdoc.one>
2020-07-05 20:15:42 +0530

    Another commit

--- FileNaming/src/main/java/com/github/nachiappannk/learn_java/runner/Program.java
+++ FileNaming/src/main/java/com/github/nachiappannk/learn_java/runner/Program.java
@@ -1,7 +1,12 @@
 package com.github.nachiappannk.learn_java.runner;
 
+import com.github.nachiappannk.learn_java.MyClass;
+
 public class Program {
     public static void main(String[] args) {
+        System.out.println(MyClass.Name);
 
+        //The below line will throw compilation error, as Another class is package private
+        //System.out.println(AnotherClass.Name);
     }
 }
