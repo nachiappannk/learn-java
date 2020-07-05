0fde76c2c8b045f7c86d100a1d6aaa02833ee867
Nachiappan NK <nachiappan.nk@mdoc.one>
2020-07-05 20:13:10 +0530

    initial version

---
+++ FileNaming/src/main/java/com/github/nachiappannk/learn_java/MyClass.java
@@ -0,0 +1,11 @@
+package com.github.nachiappannk.learn_java;
+
+
+//Naming this class as MyClass1 will throw compilation error
+public class MyClass {
+    public static String Name = "SomeValue";
+}
+
+class AnotherClass {
+    public static String Name = "SomeOtherValue";
+}
---
+++ FileNaming/src/main/java/com/github/nachiappannk/learn_java/runner/Program.java
@@ -0,0 +1,7 @@
+package com.github.nachiappannk.learn_java.runner;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
